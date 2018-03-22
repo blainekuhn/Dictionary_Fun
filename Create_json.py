4f6f6bb27237118393349f921c3aa90588152f28
blainekuhn <blainekuhn@comcast.net>
2018-03-22 16:40:44 -0700

    Builds a dynamically sized and populated Dictionary or JSON

--- Create_json.py
+++ Create_json.py
@@ -2,44 +2,44 @@
   This script is to test my ability to create a json file from a dictionary.
 
   First the script should create a dictionary
+  ifthe firs letter is 'B' add another level deep (recurse on all added)
   Then to write this to a file
   Then optional print json content to screen  
+
+  From a question I posted on StackOverflow to create the initial dictionary
+  names = [(fake.first_name(), fake.uri()) for i in range(5)]
+
+if dic found in dic[key]
+  a.append("[key]"*3)  which ends up being ['[key][key][key]']
+  this could be used to ste
+
+
 """
 
 import sys, json
-from build_a_dictionary import add_dic
-from pprint import pprint
+from faker import Faker
+fake = Faker()
 
-dic = add_dic(10)
-dic1 = {}
-dic2 = {}
-dic3 = {}
+def build_dic(dic_len, dic):
+  if isinstance(dic, (list, tuple)):
+    dic = dict(dic)
+  if isinstance(dic, dict):
+    for counter in range(len(dic)):
+      for k,v in dic.items():
+        if k[0] == 'B' or k[0] == "M":
+          update = [(fake.first_name(), fake.uri()) for i in range(5)]
+          update = dict(update)
+          dic.update({k: update})
+  return dic
 
-def build_dic(dic_len):
-  dic = add_dic(10)
-  dic1 = add_dic(dic_len)
-#  dic2 = {}
-#  dic3 = {}
-  for key in dic1.keys():
-    print(key, "\t", dic1[key])
-    dic2[key] = add_dic(8)
-  #if key in dic.keys():
-   # for key2 in dic2.keys():
-#      dic2[key][key2] = add_dic(4)
+def walk(dic):
+  for key, item in dic.items():
+      #print(type(item))
+      if isinstance(item, dict):
+        build_dic(5, item)
+        walk(item)
+  return dic
 
-
-#  for key in dic2.keys():
-#    dic3[key] = add_dic(4)
-#    for a in dic2[key1]:
-#      if dic2[key1][key1][0] == 'B':
-#        dic2[key1] = add_dic(4)
-
-        #for b in key2:
-            #if b == 'D':
-              #print("madde it here")
-              #dic3[key2] = dic2[key2] = dic2.values()
-
-
-
-build_dic(10)
-print(json.dumps(dic2, indent=4))
+a = build_dic(10, ([(fake.first_name(), fake.uri()) for i in range(10)]))
+walk(a)
+print(json.dumps(a, indent=4))
