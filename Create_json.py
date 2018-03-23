bf1fdac36316982a9d873c719c6e5e96a34dd7b2
blainekuhn <blainekuhn@comcast.net>
2018-03-23 09:49:20 -0700

    Working to depth specified but needs completion

--- Create_json.py
+++ Create_json.py
@@ -18,28 +18,51 @@ if dic found in dic[key]
 
 import sys, json
 from faker import Faker
+from random import randint
 fake = Faker()
-
+count = [0]
 def build_dic(dic_len, dic):
-  if isinstance(dic, (list, tuple)):
-    dic = dict(dic)
-  if isinstance(dic, dict):
-    for counter in range(len(dic)):
-      for k,v in dic.items():
-        if k[0] == 'B' or k[0] == "M":
-          update = [(fake.first_name(), fake.uri()) for i in range(5)]
-          update = dict(update)
-          dic.update({k: update})
+  try:
+    if isinstance(dic, (list, tuple)):
+      dic = dict(dic)
+    if isinstance(dic, dict):
+      for counter in range(len(dic)):
+        for k,v in dic.items():
+          if k[0] == 'B' or k[0] == "M":
+            update = [(fake.first_name(), fake.uri()) for i in range(dic_len)]
+            update = dict(update)
+            dic.update({k: update})
+#        count[0]+=1
+  except RecursionError:
+    print("too many iterations")
+    print(json.dumps(dic, indent=4))
+#  if count[0] > 100*(10-1):
+#    print(count)
   return dic
 
-def walk(dic):
+def walk(dic_len, dic):
+  #count[0]+=1
+  if count[0] > 10:
+    sys.exit(print(json.dumps(dic, indent=4)))
   for key, item in dic.items():
       #print(type(item))
       if isinstance(item, dict):
-        build_dic(5, item)
-        walk(item)
+        build_dic(dic_len, item)
+        walk(dic_len, item)
+        print(count)
+        count[0]+=1
   return dic
 
-a = build_dic(10, ([(fake.first_name(), fake.uri()) for i in range(10)]))
-walk(a)
-print(json.dumps(a, indent=4))
+
+dic = build_dic(10, ([(fake.first_name(), fake.uri()) for i in range(10)]))
+walk(len(dic), dic)
+print(json.dumps(dic, indent=4))
+
+"""
+good seeds to use:
+  5, 8, 300
+  11, 8, 460 s/m
+  10, 8, 460 m
+  `6, 8, 460 m
+  
+"""
